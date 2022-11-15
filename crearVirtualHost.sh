cab289b285033f71548f313487d8c0213b9a2ff2
un4rch <81522320+un4rch@users.noreply.github.com>
2022-11-15 12:23:00 +0100

    Update crearVirtualHost.sh

--- crearVirtualHost.sh
+++ crearVirtualHost.sh
@@ -1,113 +1,126 @@
-ipaddr=$(ip addr | grep "inet " | cut -d " " -f 6 | cut -d "/" -f 1 | grep -v -E "^127|^172")
-config="/etc/apache2/sites-enabled/example.conf"
-#sudo rm /ect/apahce2/sites-enabled/*
-sudo touch $config
+#!/bin/bash
 
-#rsync -av --link-dest=AYER <proyecto> HOY
-
-#ssh-keygen -t rsa -b 2048
-
-#/var/spool/cron/<crontabs>
-#sudo systemctl start/enable cronie
-#min hora diames mes diasemana ruta_script
-
-grep "SSLCertificateFile /etc/pki/tls/certs/certificado.crt" $config &> /dev/null
-if [[ "$?" == "0" ]]; then
-        echo "HTTPS VirtualHost is installed"
-else
-        echo "HTTPS VirtualHost is not Installed"
+if [[ "$(whoami)" != "root" ]]; then
+    echo -e "[!] Error: Debes ejecutar el script como usuario root. \"sudo ./init.sh\""
+    exit 1
 fi
-echo -e "\nOptions:"
-echo -e "\t[I] Install HTTPS VirtualHost"
-echo -e "\t[U] Unistall HTTPS VirtualHost"
-echo -e "\t[S] Status httpd service"
-echo -e "\t[R] Restart httpd service"
-read -p "Option:" option
 
-if [[ "$option" == "I" ]]; then
-        # Install dependencies
-        echo "[*] Instalando dependencias"
-        sudo apt install apache2 openssl -y &> /dev/null
-        sudo a2enmod ssl
-        #sudo a2enmod rewrite
+echo -e "Selecciona una opcion:\n"
+echo -e "\t[1] Instalar dependencias."
+echo -e "\t[2] Iniciar el servidor."
+echo -e "\t[3] Apagar el servidor."
+echo -e "\t[4] Reiniciar el servidor."
+echo -e "\t[5] Mostrar el estado de los servicios de la web."
+echo -e "\t[6] Obtener una shell del codigo fuente."
+echo -e "\t[7] Obtener una shell de la base de datos mysql."
+echo -e "\t[8] Inicializar la base de datos mysql."
+echo -e "\t[9] Configurar cron para realizar backups."
+echo -e "\t[10] Mostrar logs del servidor web."
+echo ""
+read -p "Opcion: " option
+echo ""
 
-        # HTML index test
-        sudo mkdir -p /var/www/html/
-        sudo sh -c 'echo "<h1>Conexion SSL</h1>" > /var/www/html/index.html'
+function instalarDependencias() {
+    sudo apt install docker docker-compose apache2 openssl -y
+}
 
-        # Give the users permisions to /var/www/html/
-        sudo chmod 777 -R /var/www/html/
+function iniciarServidor() {
+    sudo chmod 777 app/logs
+    sudo a2enmod ssl
+    /bin/cat certificados/certificado.crt &> /dev/null
+    if [[ "$?" != "0" ]]; then
+        mkdir certificados &> /dev/null
+        sudo openssl genrsa -out certificados/llave.key 2048
+        sudo openssl req -new -key certificados/llave.key -out certificados/servidor.csr
+        sudo openssl x509 -req -days 365 -in certificados/servidor.csr -signkey certificados/llave.key -out certificados/certificado.crt
+    fi
+    config="virtualhost/carshow.conf"
+    rm virtualhost/carshow.conf
+    sudo sh -c 'echo "<VirtualHost *:80>" >> '$config
+    sudo sh -c 'echo "      Redirect / https://'$(curl ifconfig.me)'" >> '$config
+    sudo sh -c 'echo "</VirtualHost>" >> '$config
+    sudo sh -c 'echo "" >> '$config
+    sudo sh -c 'echo "<VirtualHost *:443>" >> '$config
+    sudo sh -c 'echo "      SSLEngine on" >> '$config
+    sudo sh -c 'echo "      SSLCertificateFile certificados/certificado.crt" >> '$config
+    sudo sh -c 'echo "      SSLCertificateKeyFile certificados/llave.key" >> '$config
+    sudo sh -c 'echo "      SSLCertificateChainFile certificados/server.csr" >> '$config
+    sudo sh -c 'echo "      ServerAdmin example@localhost.com" >> '$config
+    sudo sh -c 'echo "      <Directory /var/www/html/>" >> '$config
+    sudo sh -c 'echo "              AllowOverride all" >> '$config
+    sudo sh -c 'echo "      </Directory>" >> '$config
+    sudo sh -c 'echo "      DocumentRoot /var/www/html/" >> '$config
+    sudo sh -c 'echo "      ErrorLog /var/log/apache2/error.log" >> '$config
+    sudo sh -c 'echo "      LogLevel warn" >> '$config
+    sudo sh -c 'echo "      CustomLog /var/log/apache2/error.log combined" >> '$config
+    sudo sh -c 'echo "</VirtualHost>" >> '$config
+    chmod +x backup.sh
+    sudo systemctl start docker
+    docker build -t="carshow" .
+    docker-compose up -d
+    docker container ls &> /dev/null
+    inicializarBaseDeDatos
+    usuario="www-data"
+    docker exec -i carshow_web_1 /bin/bash -c "chown $usuario:$usuario /var/www/html/public" &> /dev/null
+    docker exec -i carshow_web_1 /bin/bash -c "chmod -R 0755 /var/www/html/public" &> /dev/null
+    echo -e "\n[OK] Todo listo, visita la siguiente direccion en tu navegador: http://"$(curl ifconfig.me)":80/index.php"
+    #firefox "http://localhost:81/index.php"
+}
 
-        # Add VirtualHost info to httpd configuration on port 443
-        grep "SSLCertificateFile /etc/pki/tls/certs/certificado.crt" $config &> /dev/null
-        if [[ "$?" != "0"  ]]; then
-                sudo sh -c 'echo "<VirtualHost *:80>" >> '$config
-                sudo sh -c 'echo "      Redirect / https://'$(curl ifconfig.me)'" >> '$config
-                sudo sh -c 'echo "</VirtualHost>" >> '$config
-                sudo sh -c 'echo "" >> '$config
-                sudo sh -c 'echo "<VirtualHost *:443>" >> '$config
-                sudo sh -c 'echo "      SSLEngine on" >> '$config
-                sudo sh -c 'echo "      SSLCertificateFile /etc/pki/tls/certs/certificado.crt" >> '$config
-                sudo sh -c 'echo "      SSLCertificateKeyFile /etc/pki/tls/private/llave.key" >> '$config
-                sudo sh -c 'echo "      SSLCertificateChainFile /etc/pki/ca-trust/extracted/pem/server.csr" >> '$config
-                sudo sh -c 'echo "      ServerAdmin example@localhost.com" >> '$config
-                sudo sh -c 'echo "      <Directory /var/www/html/>" >> '$config
-                sudo sh -c 'echo "              AllowOverride all" >> '$config
-                sudo sh -c 'echo "      </Directory>" >> '$config
-                sudo sh -c 'echo "      DocumentRoot /var/www/html/" >> '$config
-                sudo sh -c 'echo "      ErrorLog /var/log/apache2/error.log" >> '$config
-                sudo sh -c 'echo "      LogLevel warn" >> '$config
-                sudo sh -c 'echo "      CustomLog /var/log/apache2/error.log combined" >> '$config
-                sudo sh -c 'echo "</VirtualHost>" >> '$config
-        fi
-        ls /etc/pki/tls/certs/certificado.crt &> /dev/null
-        if [[ "$?" != "0" ]]; then
-                # Remove Certificates if exists
-                sudo rm /etc/pki/tls/certs/certificado.crt &> /dev/null
-                sudo rm /etc/pki/tls/private/llave.key &> /dev/null
-                sudo rm /etc/pki/ca-trust/extracted/pem/server.csr &> /dev/null
-                # Generate new certificates
-                sudo openssl genrsa -out llave.key 2048
-                sudo openssl req -new -key llave.key -out server.csr
-                sudo openssl x509 -req -days 365 -in server.csr -signkey llave.key -out certificado.crt
-                # Move certificates to default dirs
-                sudo mkdir -p /etc/pki/tls/certs/ &> /dev/null
-                sudo mkdir -p /etc/pki/tls/private/ &> /dev/null
-                sudo mkdir -p /etc/pki/ca-trust/extracted/pem/ &> /dev/null
-                sudo mv certificado.crt /etc/pki/tls/certs/ &> /dev/null
-                sudo mv llave.key /etc/pki/tls/private/ &> /dev/null
-                sudo mv server.csr /etc/pki/ca-trust/extracted/pem/ &> /dev/null
-        fi
-        # Restart httpd server
-        sudo systemctl restart apache2
-        echo -e "\nHttps server listening on:"
-        echo -e "\t[*] https://$(curl ifconfig.me/):443 (usando la ip publica)"
-        echo -e "\t[*] https://www.ejemplo.com:443 (usando un alias)"
-        echo -e "\t[*] https://$ipaddr:443 (usando la ip privada)"
+function apagarServidor() {
+    chmod -x backup.sh
+    docker-compose down
+    rm -rf mysql 2&> /dev/null
+}
+function reiniciarServidor() {
+    apagarServidor
+    iniciarServidor
+}
 
-fi
-#Uninstall Server
-if [[ "$option" == "U" ]]; then
-        # Uninstall dependencies
-        read -p "Borrar dependencias: apache2 openssl [s/n]: " option
-        if [[ "$option" == "s" ]]; then
-                sudo apt remove apache2 openssl &> /dev/null
-        else
-                sudo systemctl restart apache2
-        fi
-        cat $config &> /dev/null
-        if [[ "$?" == "0" ]]; then
-                sudo rm $config
-        fi
-        sudo rm /etc/pki/tls/certs/certificado.crt &> /dev/null
-        sudo rm /etc/pki/tls/private/llave.key &> /dev/null
-        sudo rm /etc/pki/ca-trust/extracted/pem/server.csr &> /dev/null
-        sudo chmod 755 -R /var/www/html/
-        sudo systemctl restart apache2
-fi
-if [[ "$option" == "S" ]]; then
-        systemctl status apache2
-fi
-if [[ "$option" == "R" ]]; then
-        sudo systemctl restart apache2
-fi
+function estadoServiciosDocker() {
+    docker-compose ps
+}
+
+function obtenerShellCodigoFuente() {
+    docker exec -it carshow_web_1 /bin/bash
+}
+
+function obtenerShellMysql() {
+    docker-compose exec db mysql -uadmin -padmin1234
+}
+
+function inicializarBaseDeDatos() {
+    echo -e "[*] Cargando toda la base de datos..."
+    mysql_container_id=$(docker container ls | grep mysql | cut -d " " -f 1)
+    docker exec -i $mysql_container_id sh -c 'exec mysql -uroot -proot1234' < "$(pwd)/database/database.sql" &> /dev/null
+    while [[ "$?" == "1" ]]; do
+        docker exec -i $mysql_container_id sh -c 'exec mysql -uroot -proot1234' < "$(pwd)/database/database.sql" &> /dev/null
+    done
+    echo -e "\n[OK] Base de datos inicializada."
+}
+
+function configurarCron() {
+    crontab -e
+}
+
+function mostrarLogs() {
+    echo "Todavía no está implementado visualizar los logs..."
+}
+
+case $option in
+    1) instalarDependencias;;
+    2) iniciarServidor;;
+    3) apagarServidor;;
+    4) reiniciarServidor;;
+    5) estadoServiciosDocker;;
+    6) obtenerShellCodigoFuente;;
+    7) obtenerShellMysql;;
+    8) inicializarBaseDeDatos;;
+    9) configurarCron;;
+    10) mostrarLogs;;
+    *) echo "Opcion no valida";;
+esac
+
+#sudo groupadd docker
+#sudo usermod -aG docker $USER
+#sudo gpasswd -a $USER docker
